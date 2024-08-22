cfc50935ebce350a12f2688d6e853041ff623b37
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-22 14:08:02 +0545

    remove debug statement

--- snippets/permissions.py
+++ snippets/permissions.py
@@ -5,7 +5,6 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     """Custom permission to allow only owner to edit"""
 
     def has_object_permission(self, request, view, obj):
-        print(type(obj))
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:  # allow safe method like get
             return True
         return request.user == obj.owner
