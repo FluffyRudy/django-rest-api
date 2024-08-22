65c3923f89bfbfefdc6422048a80c58c506f3d2c
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-22 18:42:42 +0545

    Add default language and style

--- snippet/serializers.py
+++ snippet/serializers.py
@@ -6,10 +6,7 @@ class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = "__all__"
-
-    def get_initial(self):
-        initial = super().get_initial() or {}
-        initial["language"] = Snippet._meta.get_field("language").get_default()
-        initial["style"] = Snippet._meta.get_field("style").get_default()
-        print(initial)
-        return initial
+        extra_kwargs = {
+            "language": {"default": "python"},
+            "style": {"default": "monokai"},
+        }  # default laguage and style
